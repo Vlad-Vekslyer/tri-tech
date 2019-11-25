cfa87b78332a4a5ba6b2c30730876ceee710713a
unknown <vladi00345@gmail.com>
2019-11-25 14:41:45 -0800

    Fixec incorrect folder structure and implemnted basic Navbar

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import './Navbar.css';
+
+function Navbar(){
+  return(
+    <>
+      <ul>
+        <li>About</li>
+        <li>Team</li>
+        <li>Press</li>
+        <li>Contact Info</li>
+      </ul>
+    </>
+  )
+}
+
+export default Navbar;
