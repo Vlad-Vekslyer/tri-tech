75e69da90e8d9e57b5a1541357b0b129ce77f5fd
Nobu <rockmusic2090@gmail.com>
2019-11-26 15:10:05 -0800

    delete img and clean App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -8,30 +8,7 @@ import img2 from "./Article/img/img-sport.jpeg";
 import img3 from "./Article/img/img-sport.jpeg";
 
 function App() {
-  return (
-    <>
-      <Navbar />
-
-      <Article
-        title="news1"
-        date="November 25 2019"
-        author="Jon Doe"
-        img={img1}
-      />
-      <Article
-        title="news2"
-        date="October 25 2019"
-        author="James Smith"
-        img={img2}
-      />
-      <Article
-        title="news3"
-        date="May 25 2019"
-        author="Michel Smith"
-        img={img3}
-      />
-    </>
-  );
+  return <></>;
 }
 
 export default App;
