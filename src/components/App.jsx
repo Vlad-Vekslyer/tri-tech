166a39357103e7bb537c0170fa1ae5733f4fec01
Nobu <rockmusic2090@gmail.com>
2019-11-26 15:16:28 -0800

    delete a import img path

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,9 +3,6 @@ import "./App.css";
 import Navbar from "./Navbar/Navbar.jsx";
 import Article from "./Article/Article.jsx";
 import "bootstrap/dist/css/bootstrap.min.css";
-import img1 from "./Article/img/baseball-1.jpeg";
-import img2 from "./Article/img/img-sport.jpeg";
-import img3 from "./Article/img/img-sport.jpeg";
 
 function App() {
   return <></>;
