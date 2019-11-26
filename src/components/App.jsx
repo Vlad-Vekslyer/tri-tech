8b0b5480455b47a59438f5d66efc5d0b433a6b7a
Vlad-Vekslyer <vladi00345@gmail.com>
2019-11-26 15:24:26 -0800

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 import "./App.css";
-import Navbar from "./Navbar/Navbar.jsx";
-import Article from "./Article/Article.jsx";
-import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
-  return <></>;
+  return (
+    <>
+    </>
+  );
 }
 
 export default App;
