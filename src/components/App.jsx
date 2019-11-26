b72f47ab96ad10c29891178bc2f499fddd0d8e1d
Nobu <rockmusic2090@gmail.com>
2019-11-26 14:33:49 -0800

    intial commit Article component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,35 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 import Navbar from "./Navbar/Navbar.jsx";
+import Article from "./Article/Article.jsx";
+import "bootstrap/dist/css/bootstrap.min.css";
+import img1 from "./Article/img/baseball-1.jpeg";
+import img2 from "./Article/img/img-sport.jpeg";
+import img3 from "./Article/img/img-sport.jpeg";
 
 function App() {
-  return(
+  return (
     <>
-      <Navbar/>
+      <Navbar />
+
+      <Article
+        title="news1"
+        date="November 25 2019"
+        author="Jon Doe"
+        img={img1}
+      />
+      <Article
+        title="news2"
+        date="October 25 2019"
+        author="James Smith"
+        img={img2}
+      />
+      <Article
+        title="news3"
+        date="May 25 2019"
+        author="Michel Smith"
+        img={img3}
+      />
     </>
   );
 }
