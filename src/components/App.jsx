31bd66bb5a8127d152a40f3885175402221dc933
Tatsuya167 <clark.taka16@gmail.com>
2019-11-27 07:05:42 +0900

    2nd Commit: added img and made card component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,17 @@
 import React from 'react';
 import './App.css';
 import Navbar from "./Navbar/Navbar.jsx";
+import Card from "./Card/Card"
+
+// code for CArd component
+import img from "../img/card.png"
+const txt = "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Iste nesciunt et aut ipsam quos velit vitae dolor assumenda eligendi tempora, veniam modi aliquid est voluptates minus, sequi eos explicabo possimus!";
 
 function App() {
   return(
     <>
       <Navbar/>
+      <Card imageUrl={img} text={txt} name="Jhon" location="Canada" />
     </>
   );
 }
--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,12 +1,46 @@
 import React  from "react";
+import PropTypes from "prop-types";
 import "./Card.css";
 
-const Card = () => {
+const Card = (props) => {
     return (
         <>
-            <div></div>
+            <div id="Card">
+                <div className="img">
+                    {props.imageUrl && <img src={props.imageUrl} alt="person-face_img" />}
+                </div>
+                <span> 
+                    {props.name} {props.location}<br/>
+                    {props.text}
+                </span>
+            </div>
         </>
     )
 }
+
+// const Card = ({imageUrl, text, name, location}) => {
+//     return (
+//         <>
+//             <div className="Card">
+//                 <p> 
+//                     {imageUrl && <img src={imageUrl} alt="person-face_img" width="100" />}
+//                     <span>{name} {location}</span><br/>
+//                     {text}
+//                 </p>
+//             </div>
+//         </>
+//     )
+// }
+
+Card.propType = {
+    imageUrl: PropTypes.string,
+    text: PropTypes.string.isRequired,
+    name: PropTypes.string.isRequired,
+    location: PropTypes.string
+};
+
+Card.defaultProps = {
+    imageUrl: null
+};
 
 export default Card;
