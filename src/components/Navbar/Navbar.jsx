879752bcae745262dbc9ec30cb7b679f71c79fc3
unknown <vladi00345@gmail.com>
2019-11-27 16:25:06 -0800

    Added dropdown animation and logic

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,15 +1,17 @@
-import React from "react";
+import React, {useState} from "react";
 import './Navbar.css';
 import Icon from './Icon.jsx';
+import classnames from "classnames";
 
 function Navbar(){
+  let [isDrop, setDrop] = useState(false);
   return(
-    <>
+    <div>
       <ul>
         <li className="main">
           <Icon/>
         </li>
-        <li className="hamburger">
+        <li onClick={() => setDrop(!isDrop)} className="hamburger">
           <span></span>
           <span></span>
           <span></span>
@@ -23,13 +25,13 @@ function Navbar(){
           </ul>
         </li>
       </ul>
-      <ul className="dropdown">
+      <ul className={classnames('dropdown', {drop : isDrop})}>
         <li>About</li>
         <li>Team</li>
         <li>Press</li>
         <li>Contact Info</li>
       </ul>
-    </>
+    </div>
   )
 }
 
