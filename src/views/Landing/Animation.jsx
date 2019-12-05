2d289d82e0b2fea120408618c70ae66edd5e6150
unknown <vladi00345@gmail.com>
2019-12-05 15:42:56 -0800

    Animation is working properly with random delays and speeds

--- src/views/Landing/Animation.jsx
+++ src/views/Landing/Animation.jsx
@@ -1,19 +1,34 @@
 import React, {useRef, useEffect} from "react";
 
 let frame = 0;
+let reverse = false;
+let randomSpots = [null, null, null, null, null ,null];
+randomSpots = randomSpots.map(() =>
+  // X position
+ [Math.floor(Math.random() * 500),
+  // Y position
+  Math.floor(Math.random() * 350),
+  // animation speed
+  Math.floor(Math.random() * (20 - 10)  + 10),
+  // animation delay
+  Math.floor(Math.random() * 60)
+]);
 
 function animate(context){
   requestAnimationFrame(() => animate(context));
   context.clearRect(0,0, 640, 425);
-  createHexagon(context, [200, 200], 100, frame / 1.7);
-  frame++;
+  randomSpots.forEach(spot => createHexagon(context, [spot[0], spot[1]], 20, spot[2], spot[3]));
+  frame = reverse ? frame - 1 : frame + 1;
+  if(frame == 0) reverse = false;
+  else if(frame == 180) reverse = true;
 }
 
-function createHexagon(context, startPoint, size, frame){
+function createHexagon(context, startPoint, size, speedRate, offset){
   let [startX, startY] = startPoint;
+  let speed = (frame / speedRate) - (offset / speedRate);
   // each of the drawings below is a triangle that composes the hexagon
   //top left
-  let center = [startX - frame, startY - frame];
+  let center = frame > offset ? [startX - speed, startY - speed] : [startX, startY];
   context.beginPath();
   context.moveTo(center[0], center[1]);
   context.lineTo(center[0] - size, center[1]);
@@ -21,7 +36,7 @@ function createHexagon(context, startPoint, size, frame){
   context.lineTo(center[0], center[1]);
   context.stroke();
   // top middle
-  center = [startX, startY - frame];
+  center = frame > offset ? [startX, startY - speed] : [startX, startY];
   context.beginPath();
   context.moveTo(center[0], center[1]);
   context.lineTo(center[0] - (size / 2), center[1] - size);
@@ -29,7 +44,7 @@ function createHexagon(context, startPoint, size, frame){
   context.lineTo(center[0], center[1]);
   context.stroke();
   // top right
-  center = [startX + frame, startY - frame];
+  center = frame > offset ? [startX + speed, startY - speed] : [startX, startY];
   context.beginPath();
   context.moveTo(center[0], center[1]);
   context.lineTo(center[0] + size, center[1]);
@@ -37,7 +52,7 @@ function createHexagon(context, startPoint, size, frame){
   context.lineTo(center[0], center[1]);
   context.stroke();
   // bottom right
-  center = [startX + frame, startY + frame];
+  center = frame > offset ? [startX + speed, startY + speed] : [startX, startY];
   context.beginPath();
   context.moveTo(center[0], center[1]);
   context.lineTo(center[0] + size, center[1]);
@@ -45,7 +60,7 @@ function createHexagon(context, startPoint, size, frame){
   context.lineTo(center[0], center[1]);
   context.stroke();
   // bottom middle
-  center = [startX, startY + frame];
+  center = frame > offset ? [startX, startY + speed] : [startX, startY];
   context.beginPath();
   context.moveTo(center[0], center[1]);
   context.lineTo(center[0] - (size / 2), center[1] + size);
@@ -53,7 +68,7 @@ function createHexagon(context, startPoint, size, frame){
   context.lineTo(center[0], center[1]);
   context.stroke();
   // bottom left
-  center = [startX - frame, startY + frame];
+  center = frame > offset ? [startX - speed, startY + speed] : [startX, startY];
   context.beginPath();
   context.moveTo(center[0], center[1]);
   context.lineTo(center[0] - size, center[1]);
