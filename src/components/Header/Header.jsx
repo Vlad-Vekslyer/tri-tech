8d0f862ff52b5fd50e8f51383ce39d84f31bfe7c
Isao Kozakai <feat130@hotmail.co.jp>
2019-12-04 21:24:42 -0800

    Add the prpo-types to Header component and Fix the problem with centering

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,15 +1,27 @@
 import React from 'react';
 import classNames from 'classnames';
+import PropTypes from "prop-types";
 import './Header.css';
 
 const Header = ({ position, large, underline, children }) => {
     return (
-        <div className={classNames("header-container", position, { "large": large }, { "underline": underline })}>
-            <h1>
+        <div className={classNames("header-container", position)}>
+            <h1 className={classNames({ "large": large }, { "underline": underline })}>
                 {children}
             </h1>
         </div>
     );
 }
+
+Header.propTypes = {
+    position: PropTypes.string.isRequired,
+    large: PropTypes.bool,
+    underline: PropTypes.bool
+};
+
+Header.defaultProps = {
+    large: false,
+    underline: false
+};
 
 export default Header;
