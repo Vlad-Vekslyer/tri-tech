cdb48d66d61d1318b223608bd0e0109d8ff7fed6
unknown <vladi00345@gmail.com>
2019-12-06 16:46:04 -0800

    Adjusted delays and speedrates of hexagon animations. Added gradients to the hexagons

--- src/views/Landing/Animation.jsx
+++ src/views/Landing/Animation.jsx
@@ -9,9 +9,9 @@ let hexagonProps = [];
 hexagonProps.push({
   startX: 400,
   startY: 250,
-  speedRate: 15,
+  speedRate: 8,
   size: 150,
-  delay: 20
+  delay: 80
 });
 
 hexagonProps.push({
@@ -19,7 +19,7 @@ hexagonProps.push({
   startY: 150,
   speedRate: 15,
   size: 50,
-  delay: 40
+  delay: 50
 });
 
 hexagonProps.push({
@@ -27,7 +27,7 @@ hexagonProps.push({
   startY: 75,
   speedRate: 25,
   size: 50,
-  delay: 20
+  delay: 10
 });
 
 hexagonProps.push({
@@ -35,7 +35,7 @@ hexagonProps.push({
   startY: 450,
   speedRate: 15,
   size: 30,
-  delay: 30
+  delay: 10
 });
 
 hexagonProps.push({
@@ -43,7 +43,7 @@ hexagonProps.push({
   startY: 500,
   speedRate: 15,
   size: 30,
-  delay: 40
+  delay: 50
 });
 
 hexagonProps.push({
@@ -51,7 +51,7 @@ hexagonProps.push({
   startY: 450,
   speedRate: 30,
   size: 30,
-  delay: 50
+  delay: 80
 });
 
 
@@ -61,7 +61,7 @@ function animate(context, props){
   hexagonProps.forEach(props => createHexagon(context, props));
   frame = reverse ? frame - 1 : frame + 1;
   if(frame == 0) reverse = false;
-  else if(frame == 180) reverse = true;
+  else if(frame == 200) reverse = true;
 }
 
 function createHexagon(context, props){
@@ -71,58 +71,82 @@ function createHexagon(context, props){
 
   // top left
   // don't animate if the frame is not past the delay value
-  let center = frame > delay ? [startX - speed, startY - speed] : [startX, startY];
+  let center = frame > delay ? [startX - speed * 1.5, startY - speed] : [startX, startY];
   context.beginPath();
   context.moveTo(center[0], center[1]);
   context.lineTo(center[0] - size, center[1]);
   context.lineTo(center[0] - (size / 2), center[1] - size);
   context.lineTo(center[0], center[1]);
-  context.stroke();
+  let gradient = context.createLinearGradient(center[0] - (size / 2), center[1] - size, center[0] - (size / 2), center[1] + size * 1.5);
+  gradient.addColorStop(0, "#3898EC");
+  gradient.addColorStop(1, "rgba(255,255,255,0.6)");
+  context.fillStyle = gradient;
+  context.fill();
 
   // top middle
-  center = frame > delay ? [startX, startY - speed] : [startX, startY];
+  center = frame > delay ? [startX, startY - speed * 1.3] : [startX, startY];
   context.beginPath();
   context.moveTo(center[0], center[1]);
   context.lineTo(center[0] - (size / 2), center[1] - size);
   context.lineTo(center[0] - (size / 2) + size, center[1] - size);
   context.lineTo(center[0], center[1]);
-  context.stroke();
+  gradient = context.createLinearGradient(center[0], center[1] - size, center[0], center[1] + size * 2.5);
+  gradient.addColorStop(0, "#2585D9");
+  gradient.addColorStop(1, "rgba(255,255,255,0.6)");
+  context.fillStyle = gradient;
+  context.fill();
 
   // top right
-  center = frame > delay ? [startX + speed, startY - speed] : [startX, startY];
+  center = frame > delay ? [startX + speed * 1.5, startY - speed] : [startX, startY];
   context.beginPath();
   context.moveTo(center[0], center[1]);
   context.lineTo(center[0] + size, center[1]);
   context.lineTo(center[0] + (size / 2), center[1] - size);
   context.lineTo(center[0], center[1]);
-  context.stroke();
+  gradient = context.createLinearGradient(center[0] + (size / 2), center[1] - size, center[0] + (size / 2), center[1] + size * 2.5);
+  gradient.addColorStop(0, "#0D64B1");
+  gradient.addColorStop(1, "rgba(255,255,255,0.6)");
+  context.fillStyle = gradient;
+  context.fill();
 
   // bottom right
-  center = frame > delay ? [startX + speed, startY + speed] : [startX, startY];
+  center = frame > delay ? [startX + speed * 1.5, startY + speed] : [startX, startY];
   context.beginPath();
   context.moveTo(center[0], center[1]);
   context.lineTo(center[0] + size, center[1]);
   context.lineTo(center[0] + (size / 2), center[1] + size);
   context.lineTo(center[0], center[1]);
-  context.stroke();
+  gradient = context.createLinearGradient(center[0] + (size / 2), center[1], center[0] + (size / 2), center[1] + size * 3);
+  gradient.addColorStop(0, "#3898EC");
+  gradient.addColorStop(1, "rgba(255,255,255,0.6)");
+  context.fillStyle = gradient;
+  context.fill();
 
   // bottom middle
-  center = frame > delay ? [startX, startY + speed] : [startX, startY];
+  center = frame > delay ? [startX, startY + speed * 1.3] : [startX, startY];
   context.beginPath();
   context.moveTo(center[0], center[1]);
   context.lineTo(center[0] - (size / 2), center[1] + size);
   context.lineTo(center[0] - (size / 2) + size, center[1] + size);
   context.lineTo(center[0], center[1]);
-  context.stroke();
+  gradient = context.createLinearGradient(center[0], center[1], center[0], center[1] + size * 3);
+  gradient.addColorStop(0, "#79C1FF");
+  gradient.addColorStop(1, "rgba(255,255,255,0.6)");
+  context.fillStyle = gradient;
+  context.fill();
 
   // bottom left
-  center = frame > delay ? [startX - speed, startY + speed] : [startX, startY];
+  center = frame > delay ? [startX - speed * 1.5, startY + speed] : [startX, startY];
   context.beginPath();
   context.moveTo(center[0], center[1]);
   context.lineTo(center[0] - size, center[1]);
   context.lineTo(center[0] - (size / 2), center[1] + size);
   context.lineTo(center[0], center[1]);
-  context.stroke();
+  gradient = context.createLinearGradient(center[0] - (size / 2), center[1], center[0] - (size / 2), center[1] + size * 3);
+  gradient.addColorStop(0, "#52AAF6");
+  gradient.addColorStop(1, "rgba(255,255,255,0.6)");
+  context.fillStyle = gradient;
+  context.fill();
 }
 
 function Animation(props){
