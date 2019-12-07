7e02505ca1bf1dc795cf73ac209106dd2b3391ec
Isao Kozakai <>
2019-12-06 16:44:13 -0800

    Add dummy data for each article and Fix the problem with the scroll up behavior

--- src/components/Press/Article/Article.jsx
+++ src/components/Press/Article/Article.jsx
@@ -3,22 +3,20 @@ import "./Article.scss";
 
 const Article = ({ img, title, date, author }) => {
   return (
-    <div>
-      <div className="content card">
-        <img className="card-img-top" src={img} alt="Card" />
-        <div className="card-body">
-          <h3 className="card-title">{title}</h3>
-          <p>
-            {date} by {author}
-          </p>
-          <p className="card-text">
-            Some quick example text to build on the card title and make up the
-            bulk of the card's content.
-          </p>
-          <a href="#" className="btn btn-primary">
-            Read more
-          </a>
-        </div>
+    <div className="content card">
+      <img className="card-img-top" src={img} alt="Card" />
+      <div className="card-body">
+        <h3 className="card-title">{title}</h3>
+        <p>
+          {date} by {author}
+        </p>
+        <p className="card-text">
+          Some quick example text to build on the card title and make up the
+          bulk of the card's content.
+        </p>
+        <a href="#" className="btn btn-primary">
+          Read more
+        </a>
       </div>
     </div>
   );
--- src/views/Press/Press.jsx
+++ src/views/Press/Press.jsx
@@ -1,29 +1,32 @@
-import React, { useState, useRef } from 'react';
-import classNames from 'classnames';
+import React, { useState, useRef, useEffect } from 'react';
 import Article from '../../components/Press/Article/Article';
 import ArticlesToggler from '../../components/Press/ArticlesToggler/ArticlesToggler';
-import './Press.css';
+import './Press.scss';
 
 const Press = () => {
     const [isOpen, setIsOpen] = useState(false);
     const [isAbsolute, setIsAbsolute] = useState(false);
-    const [articles, setArticles] = useState([1, 2, 3, 4, 5, 6]);
+    const articles = require('./dummy-data.json').dummy;;
     const pressSection = useRef();
+    const articlesDiv = useRef();
+
+    const maxHeight = isOpen ? articlesDiv.current.scrollHeight + "px" : "";
 
     document.onscroll = () => {
-        setIsAbsolute(isOpen && (window.pageYOffset < pressSection.current.offsetTop - 315));
+        setIsAbsolute(isOpen && (window.pageYOffset < pressSection.current.offsetTop - 525));
     }
 
     const handleClickToggler = () => {
         setIsOpen(!isOpen);
     }
+
     return (
         <section id="press" ref={pressSection}>
-            <div className={classNames("grid-container", (isOpen ? "show" : "collapse"))}>
+            <div ref={articlesDiv} className="grid-container" style={{ maxHeight }}>
                 {
-                    articles.map((article) => {
+                    articles.map(({ img, title, date, author }) => {
                         return (
-                            <Article style={{ height: "300" }} />
+                            <Article img={img} title={title} date={date} author={author} />
                         )
                     })
                 }
