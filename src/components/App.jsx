64433cb0a02e3da3f20cc8f1fdc386ad6be1a502
unknown <vladi00345@gmail.com>
2019-12-10 14:15:24 -0800

    Integrated most of the pages together. Fixed minor styling conflicts

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,17 +5,16 @@ import AdvisoryPage from "../views/AdvisoryPage/AdvisoryPage.jsx";
 import Navbar from "./Navbar/Navbar.jsx";
 import Landing from "../views/Landing/Landing.jsx";
 import AboutUs from "../views/AboutUs/AboutUs.jsx";
+import Press from "../views/Press/Press.jsx";
 
 function App() {
   return (
     <>
-<<<<<<< HEAD
       <Navbar/>
       <Landing/>
-=======
-      <Navbar />
-      <AdvisoryPage />
->>>>>>> master
+      <AboutUs/>
+      <AdvisoryPage/>
+      <Press/>
     </>
   );
 }
