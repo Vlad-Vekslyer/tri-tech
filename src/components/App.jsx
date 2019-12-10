35ca5d384edbfb0852cb10e5895c88abc6dbe6fe
Nobu <rockmusic2090@gmail.com>
2019-12-10 11:12:33 -0800

    changed inside  AboutUs.scss

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,8 +1,12 @@
 import React from "react";
 import "./App.scss";
-
+import AboutUs from "../views/AboutUs/AboutUs";
 function App() {
-  return <></>;
+  return (
+    <>
+      <AboutUs />
+    </>
+  );
 }
 
 export default App;
