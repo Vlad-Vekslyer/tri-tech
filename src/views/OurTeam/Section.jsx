4ba791c901640d47c1ad868353cbfb84dd6ccdd1
unknown <vladi00345@gmail.com>
2019-12-13 15:49:49 -0800

    Added dropdown logic and button styling

--- src/views/OurTeam/Section.jsx
+++ src/views/OurTeam/Section.jsx
@@ -1,15 +1,32 @@
-import React, {useState} from "react";
-import classNames from "classnames";
+import React, {useRef} from "react";
 
 import "./OurTeam.scss";
 import img from "../../assets/AboutUs-img.png";
 
-const Section = (props) => {
-  // decides whether to hide extra text in the mobile or not
-  let [isHidden, setHidden] = useState(true);
+async function sleep(ms){
+  return new Promise((resolve) => setTimeout(() => resolve(), ms))
+}
 
+const Section = (props) => {
+  let desktop = useRef(null);
+  let mobile = useRef(null);
   // get button-related markup and logic
-  const getButton = () => (<button onClick={() => setHidden(!isHidden)}>{isHidden ? "Expand" : "Collapse"}</button>);
+  const getButton = () => (
+    <button onClick={async () => {
+      if(desktop.current.classList.contains("drop")) {
+        desktop.current.classList.remove("drop");
+        await sleep(500);
+        desktop.current.classList.toggle("mobile-hidden");
+        mobile.current.classList.toggle("mobile-hidden");
+      } else {
+        desktop.current.classList.toggle("mobile-hidden");
+        mobile.current.classList.toggle("mobile-hidden");
+        setTimeout(() => desktop.current.classList.add("drop"), 20);
+      }
+
+    }}>
+    Toggle</button>
+  );
   return(
     <div className="ourTeam-content">
       <div className="ourTeam-img-section">
@@ -20,11 +37,11 @@ const Section = (props) => {
           {props.name}, <span className="ourTeam-location">{props.location}</span>
         </p>
         {/* can appear only on a mobile view when isHidden is true */}
-        <div className={classNames({"mobile-hidden" : !isHidden}, "mobile-revealed")}>
+        <div ref={mobile} className="mobile-only">
           {props.revealed}
         </div>
         {/* will always appear on a desktop view regardless of state */}
-        <div className={classNames({"mobile-hidden" : isHidden})}>
+        <div ref={desktop} className="mobile-hidden">
           {props.hidden}
         </div>
         {getButton()}
